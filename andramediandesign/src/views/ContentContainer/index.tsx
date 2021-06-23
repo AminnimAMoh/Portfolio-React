fef5fdcfecd42cd74fbaf3db224b1f82a4dc01fb
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-06-23 11:09:02 +1000

    Rush update

--- andramediandesign/src/views/ContentContainer/index.tsx
+++ andramediandesign/src/views/ContentContainer/index.tsx
@@ -5,18 +5,29 @@ import { RootState } from 'src/store'
 import UX from '../Pages/UX'
 
 interface Props {
-  
+  state: string | null
+}
+
+const RenderObject({state}: Props): React.ReactElement{
+  switch(state){
+    case '':
+      break;
+      default: return <UX/>
+      break;
+  }
+  return <div></div>
 }
 
 function ContentContainer({}: Props): React.ReactElement {
 const classes=useStyle()
 const buttonState: boolean=useSelector((state: RootState) =>state.buttonAction.containerState)
+const renderObject: string=useSelector((state: RootState) =>state.buttonAction.renderPage)
   return (
     <div 
     className={classes.root}
     style={buttonState ? {width: '100%'} : {}}
     >
-      <UX/>
+      <RenderObject state={renderObject} />
     </div>
   )
 }
