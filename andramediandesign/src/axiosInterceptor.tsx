2f689d412efccdbf1876ad61666f77eb7972bd45
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-07-12 14:55:09 +1000

    Port must be fixed on app not on the base.

--- andramediandesign/src/axiosInterceptor.tsx
+++ andramediandesign/src/axiosInterceptor.tsx
@@ -1,25 +1,27 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
+// import axios, { AxiosError, AxiosResponse } from "axios";
 
-export default () => {
-  const BASE_DESTINATION = process.env.REACT_APP_BACKEND;
-  const headers = { "Content-Type": "application/json" };
+// export default () => {
 
-  const axiosInterceptor = axios.create({
-    BASE_DESTINATION,
-    timeout: 1000,
-    headers,
-  });
+//   const headers = { "Content-Type": "application/json" };
 
-  const onResponse = (response: AxiosResponse): AxiosResponse => {
-    console.log(response);
-    return response;
-  };
-  const onResponseError = (error: AxiosError): AxiosError => {
-    console.log(error);
-    return error;
-  };
+//   const axiosInterceptor = axios.create({
+//     BASE_DESTINATION:'http://localhost:3000/',
+//     timeout: 1000,
+//     headers,
+//   });
 
-  axiosInterceptor.interceptors.response.use(onResponse, onResponseError);
+//   const onResponse = (response: AxiosResponse): AxiosResponse => {
+//     console.log(response);
+//     return response;
+//   };
+//   const onResponseError = (error: AxiosError): AxiosError => {
+//     console.log(error);
+//     return error;
+//   };
 
-  return axiosInterceptor;
-};
+//   axiosInterceptor.interceptors.response.use(onResponse, onResponseError);
+
+//   return axiosInterceptor;
+// };
+
+export default null
--- andramediandesign/src/redux/slices/fetchSlice.tsx
+++ andramediandesign/src/redux/slices/fetchSlice.tsx
@@ -1,14 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "src/store";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
 
-import axiosInterceptor from "../../axiosInterceptor";
+// import axiosInterceptor from "../../axiosInterceptor";
 
 export const fetchAnnualrainData=createAsyncThunk(
     'fetchData/AnnualRainData',
     async (_, thunkAPI) => {
         try{
-            const response=axiosInterceptor().get('annual_rain')
+            const response=axios.get('http://localhost:3000/annual_rain')
             return response
         }catch (err){
             return thunkAPI.rejectWithValue(err)
