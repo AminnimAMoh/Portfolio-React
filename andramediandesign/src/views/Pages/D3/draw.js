139bb34ffc29862e299fcf3f081620a15a1834be
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-07-22 21:30:39 +1000

    Slums graph fixed and unified.

--- andramediandesign/src/views/Pages/D3/draw.js
+++ andramediandesign/src/views/Pages/D3/draw.js
@@ -4,7 +4,8 @@ import style from "./style.css";
 
 import PopulationCircles from "./MapComponents/PopulationCircles";
 import removeEllipses from "./MapComponents/RemoveEllipses";
-import slumsComponent from './MapComponents/SlumsComponent'
+import slumsComponent from './MapComponents/SlumsComponent';
+import DrawAll from './MapComponents/DrawAllComponents'
 
 import PThreeYears from "./data/PThreeYears.csv";
 import MonthFiveYears from "./data/Month-FiveYears.csv";
@@ -611,7 +612,7 @@ export const draw = (
         removeFunction();
         onClickTextFunction(this,yearsContainer);
         const yearListSelected = this.id;
-        drawAll(yearListSelected);
+        DrawAll(annualrain,yearListSelected, yearSelected, firstMin, firstMax, radScale, cityCircles, legendGraph);
       });
 
     function removeFunction() {
@@ -684,99 +685,9 @@ export const draw = (
       cityLables
     );
 
-    function drawAll(year) {
-      const annualRainData = annualrain.data;
-      const dataSet = annualRainData.map((properties) => {
-        return properties[`Sum${year}`];
-      });
-      yearSelected = year;
+    DrawAll(annualrain,year, yearSelected, firstMin, firstMax, radScale, cityCircles, legendGraph)
 
-      let circleTransition = d3.transition().ease(d3.easeExp).duration(1000);
 
-      firstMin = d3.min(dataSet);
-      firstMax = d3.max(dataSet);
-
-      radScale = d3.scaleLinear().domain([firstMin, firstMax]).range([4, 24]);
-
-      let selectContainerCircles = cityCircles
-        .selectAll("circle")
-        .data(dataSet);
-
-      selectContainerCircles.exit().remove();
-
-      selectContainerCircles.enter().append("circle").attr("r", 0);
-
-      selectContainerCircles
-        .select("circle")
-        .data(dataSet)
-        .enter()
-        .selectAll("circle")
-        .attr("class", "cities-circles")
-        .transition()
-        .duration(500)
-        .attr("r", function (d) {
-          return radScale(d);
-        });
-
-      let managedArray = [];
-      let sortedData = dataSet.sort(d3.descending);
-      managedArray.push(sortedData[0]);
-      managedArray.push(sortedData[sortedData.length / 2]);
-      managedArray.push(sortedData[sortedData.length - 1]);
-
-      const legendGraphLines = legendGraph.selectAll("line").data(managedArray);
-      legendGraphLines.exit().remove();
-
-      const legendGraphCircle = legendGraph
-        .selectAll("circle")
-        .data(managedArray);
-      legendGraphCircle.exit().remove();
-
-      const legendGraphText = legendGraph.selectAll("text").data(managedArray);
-      legendGraphText.exit().remove();
-
-      legendGraphCircle
-        .select("circle")
-        .data(managedArray)
-        .enter()
-        .selectAll("circle")
-        .transition()
-        .duration(500)
-        .attr("transform", function (d, i) {
-          return "translate(0," + -radScale(d) + ")";
-        })
-        .attr("r", function (d) {
-          return radScale(d);
-        });
-
-      legendGraphLines
-        .select("line")
-        .data(managedArray)
-        .enter()
-        .selectAll("line")
-        .transition()
-        .duration(500)
-        .attr("transform", function (d, i) {
-          return "translate(0," + -radScale(d) * 2 + ")";
-        });
-
-      legendGraphText
-        .select("text")
-        .data(managedArray)
-        .enter()
-        .selectAll("text")
-        .attr("transform", function (d, i) {
-          return (
-            "translate(" + (i * 50 + 52) + "," + (-radScale(d) * 2 - 3) + ")"
-          );
-        })
-        .text((d) => {
-          console.log(d);
-          return d + "mm";
-        })
-        .style("font-size", "6pt")
-        .style("fill", "white");
-    }
     d3.selectAll("g").raise();
 
     window.addEventListener("click", function (event) {
